83ed0e1750e2191d3b2c274fade955d5042e5fae
ethan <ethan@LAPTOP-O9MCJS4B>
2021-03-19 11:33:24 -0400

    first commit

---
+++ DLLTaskManager/DNode.cpp
@@ -0,0 +1,27 @@
+/*
+ * DNode.cpp
+ *
+ *  Created on: Apr 2, 2020
+ *      Author: 13027
+ */
+
+#include <stdlib.h>
+#include <iostream>
+#include "DNode.hpp"
+using namespace std;
+
+
+DNode::DNode(string t, int p, int h, int m){
+/*You need to write this!!!! */
+}
+
+
+DNode::DNode(){
+	task = NULL;
+	next = NULL;
+	prev = NULL;
+}
+
+
+
+
